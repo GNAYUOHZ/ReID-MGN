f0251e9e6003ec6f2c3fbc8ce5741d21436c20cf
GNAYUOHZ <zhouyang12345@outlook.com>
2019-11-15 09:51:50 +0800

    easy to understand

--- utils/extract_feature.py
+++ utils/extract_feature.py
@@ -6,14 +6,16 @@ def extract_feature(model, loader):
 
     for (inputs, labels) in loader:
 
-        ff = torch.FloatTensor(inputs.size(0), 2048).zero_()
-        for i in range(2):
-            if i == 1:
-                inputs = inputs.index_select(3, torch.arange(inputs.size(3) - 1, -1, -1).long())
-            input_img = inputs.to('cuda')
-            outputs = model(input_img)
-            f = outputs[0].data.cpu()
-            ff = ff + f
+        input_img = inputs.to('cuda')
+        outputs = model(input_img)
+        f1 = outputs[0].data.cpu()
+
+        # flip
+        inputs = inputs.index_select(3, torch.arange(inputs.size(3) - 1, -1, -1))
+        input_img = inputs.to('cuda')
+        outputs = model(input_img)
+        f2 = outputs[0].data.cpu()
+        ff = f1 + f2
 
         fnorm = torch.norm(ff, p=2, dim=1, keepdim=True)
         ff = ff.div(fnorm.expand_as(ff))
